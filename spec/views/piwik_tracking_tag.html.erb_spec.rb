94bb778c88a83367d5396fa4566c6f9ebd32d857
Bèr Kessels <ber@webschuur.com>
2012-12-11 18:43:59 +0100

    Refactor spec to re-use local variables, using let()

--- spec/views/piwik_tracking_tag.html.erb_spec.rb
+++ spec/views/piwik_tracking_tag.html.erb_spec.rb
@@ -1,6 +1,9 @@
 require "erb"
 
 describe "views/piwik_tracking_tag.html.erb" do
+  let(:url) { nil }
+  let(:id_site) { nil }
+
   before(:each) do
     filename = File.join(File.dirname(__FILE__), '..', '..', 'app', 'views', 'piwik_analytics', 'piwik_tracking_tag.html.erb')
     @template = ERB.new(File.read(filename))
@@ -8,12 +11,10 @@ describe "views/piwik_tracking_tag.html.erb" do
 
   it "should render url" do
     url = "example.com"
-    id_site = nil
     @template.result(binding).should =~ /http:\/\/example.com/
   end
 
   it "should render id_site" do
-    url = nil
     id_site = 4
     @template.result(binding).should =~ /"piwik\.php", 4/
   end
