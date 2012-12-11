b70242b291d115b18b4656b1360daeab5bafa556
Bèr Kessels <ber@webschuur.com>
2012-12-11 18:58:43 +0100

    Pass tracker to allow rendering dociment_title

--- spec/views/piwik_tracking_tag.html.erb_spec.rb
+++ spec/views/piwik_tracking_tag.html.erb_spec.rb
@@ -1,8 +1,10 @@
 require "erb"
+require "piwik_analytics"
 
 describe "views/piwik_tracking_tag.html.erb" do
   let(:url) { nil }
   let(:id_site) { nil }
+  let(:tracker) { PiwikAnalytics.piwik_tracker }
 
   before(:each) do
     filename = File.join(File.dirname(__FILE__), '..', '..', 'app', 'views', 'piwik_analytics', 'piwik_tracking_tag.html.erb')
@@ -17,5 +19,31 @@ describe "views/piwik_tracking_tag.html.erb" do
   it "should render id_site" do
     id_site = 4
     @template.result(binding).should =~ /"piwik\.php", 4/
+  end
+
+  context "tracker" do
+    before(:each) do
+      @tracker = PiwikAnalytics.piwik_tracker
+    end
+    context "document title is set" do
+      before(:each) do
+        @tracker.document_title = "FooBar"
+      end
+      it "should render document_title" do
+        # Usually the render-helper calls this and passes along to render
+        tracker = PiwikAnalytics.piwik_tracker
+        @template.result(binding).should =~ /piwikTracker\.setDocumentTitle\("FooBar"\);/
+      end
+    end
+
+    context "document title is not set" do
+      before(:each) do
+        @tracker.document_title = nil
+      end
+      it 'should not render dociment_title' do
+        tracker = PiwikAnalytics.piwik_tracker
+        @template.result(binding).should_not =~ /setDocumentTitle/
+      end
+    end
   end
 end
